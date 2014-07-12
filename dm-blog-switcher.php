b714233f831fff46582339e32e6194837f184770
Bradford Knowlton <brad@ucanbonline.net>
2014-07-12 12:51:20 -0600

    Added Github Version Details

--- dm-blog-switcher.php
+++ dm-blog-switcher.php
@@ -3,11 +3,13 @@
 Plugin Name: DM Blog Switcher
 Plugin URI: http://designmissoula.com/
 Description: Used by millions, to make WordPress Better. Uses simple shortcode to switch between blogs on a multi blog network.
-Version: 3.0.0
+Version: 3.0.2
 Author: Bradford Knowlton
 Author URI: http://bradknowlton.com
 License: GPLv2 or later
 Text Domain: blog-switcher
+GitHub Plugin URI: https://github.com/DesignMissoula/DM-blog-switcher
+GitHub Branch:     master
 */
 
 function blog_switch_function( $atts ){

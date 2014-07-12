0fa0bb34baa6272f251d234576c8bdc82b8d40cc
Bradford Knowlton <brad@ucanbonline.net>
2014-07-12 12:02:49 -0600

    Initial commit

---
+++ dm-blog-switcher.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Plugin Name: DM Blog Switcher
+Plugin URI: http://designmissoula.com/
+Description: Used by millions, to make WordPress Better. Uses simple shortcode to switch between blogs on a multi blog network.
+Version: 3.0.0
+Author: Bradford Knowlton
+Author URI: http://bradknowlton.com
+License: GPLv2 or later
+Text Domain: blog-switcher
+*/
+
+function blog_switch_function( $atts ){
+	switch_to_blog(15);
+
+	return '';
+}
+
+add_shortcode( 'blog-switch', 'blog_switch_function' );
+
+
+
+
+
+function blog_restore_function( $atts ){
+	restore_current_blog();
+	return '';
+}
+
+add_shortcode( 'blog-restore', 'blog_restore_function' );
